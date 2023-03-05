eb382428deeffb9117aa951cee488ec5b183a99f
William Smith <smudge-smiff@users.noreply.github.com>
2023-03-05 21:53:46 +0000

    Update UartToI2c.ino

--- UartToI2c.ino
+++ UartToI2c.ino
@@ -1,5 +1,5 @@
 #include <TinyGPSPlus.h>
-#include<Wire.h>
+#include <Wire.h>
 #include "uniondef.h"
 
 #define Address 0x03
@@ -9,16 +9,17 @@
 HardwareSerial gpsSerial(PA3, PA2);
 HardwareSerial debugUART(PA12, PA11);
 
-byte dat[128]= {0};
+byte dat[128] = {0};
 uint8_t reg_to_read = 0;
 TinyGPSPlus gps;
 uint32Union_t x;
 
-unsigned long start_time; 
+unsigned long start_time;
 unsigned long updateTime;
 unsigned long current_time;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   x.value = 20;
   debugUART.begin(57600);
@@ -31,87 +32,100 @@ void setup() {
   Wire.onRequest(requestEvent);
 
   updateTime = 50; // after 10 ms trigger the event
-	current_time = millis();
-	start_time = current_time;
+  current_time = millis();
+  start_time = current_time;
 }
 
-void loop() {
-  while (gpsSerial.available() > 0){
-    if (gps.encode(gpsSerial.read())){
-
+void loop()
+{
+  while (gpsSerial.available() > 0)
+  {
+    if (gps.encode(gpsSerial.read()))
+    {
     }
   }
 
   current_time = millis(); // update the timer every cycle
 
-	if (current_time - start_time >= updateTime) {
-		start_time = current_time;  // reset the timer
+  if (current_time - start_time >= updateTime)
+  {
+    start_time = current_time; // reset the timer
     hasFix();
     updateDataBuffer();
     digitalWrite(LED_PIN, !hasFix());
-	}
+  }
 }
 
-void receiveEvent(int bytes){
+void receiveEvent(int bytes)
+{
   byte recieved[bytes];
   int i = 0;
-  while (0 < Wire.available()) {//Read all bytes
+  while (0 < Wire.available())
+  { // Read all bytes
     recieved[i] = Wire.read();
     i++;
   }
   reg_to_read = recieved[0];
-  if (sizeof(recieved)>=2){
-    dat[reg_to_read]=recieved[1]; // Update the reigster with the second byte recieved, disgard all other data
+  if (sizeof(recieved) >= 2)
+  {
+    dat[reg_to_read] = recieved[1]; // Update the reigster with the second byte recieved, disgard all other data
   }
 }
 
-void requestEvent() {
-  for (int i = reg_to_read; i< sizeof(dat); i++){
-    if (i >= sizeof(dat)){
+void requestEvent()
+{
+  for (int i = reg_to_read; i < sizeof(dat); i++)
+  {
+    if (i >= sizeof(dat))
+    {
       Wire.write(0x00);
-    } else {
+    }
+    else
+    {
       Wire.write(dat[i]);
     }
   }
 }
 
-void updateDataBuffer(){
+void updateDataBuffer()
+{
   dat[0] = 0b00000000; // Reserved for GPS baud rate functionality
   dat[1] = 0b00000000; // Reserved for status information
-  
+
   doubleUnion_t lat;
   lat.value = gps.location.lat();
-  loadBytes(lat.bytes,2);// bytes 2 - 5
+  loadBytes(lat.bytes, 2); // bytes 2 - 5
 
   doubleUnion_t lng;
   lat.value = gps.location.lng();
-  loadBytes(lng.bytes,6);// bytes 6 - 9
+  loadBytes(lng.bytes, 6); // bytes 6 - 9
 
   doubleUnion_t alt;
   alt.value = gps.altitude.meters();
-  loadBytes(alt.bytes,10);// bytes 10-13
+  loadBytes(alt.bytes, 10); // bytes 10-13
 
   doubleUnion_t course;
   course.value = gps.course.deg();
-  loadBytes(course.bytes,14);// bytes 14-17
-  
+  loadBytes(course.bytes, 14); // bytes 14-17
+
   uint32Union_t sat;
-  sat.value = gps.satellites.value();// No 
-  dat[18] = sat.bytes[0]; // No need to send all four bytes, satellites will never be greater than 128
+  sat.value = gps.satellites.value(); // No
+  dat[18] = sat.bytes[0];             // No need to send all four bytes, satellites will never be greater than 128
 
   int32Union_t hdop;
   hdop.value = gps.hdop.value();
-  loadBytes(hdop.bytes, 19);// 19 - 22
+  loadBytes(hdop.bytes, 19); // 19 - 22
 }
 
-void loadBytes(byte data_to_load[], int start_byte){
+void loadBytes(byte data_to_load[], int start_byte)
+{
   int howMany = sizeof(data_to_load);
   int end_byte = start_byte + howMany - 1;
   int j = 0;
-  for (int i=start_byte; i<= end_byte; i++){
+  for (int i = start_byte; i <= end_byte; i++)
+  {
     dat[i] = data_to_load[j];
     j++;
-    
   }
 }
 /*
@@ -135,15 +149,19 @@ void statusOfIBit(byte test, int bit){
 /*
   Can be assumed, if the age of the relevant positions exceed, 1.5s then the GPS has lost fix and is no longer active
 */
-bool hasFix(){
+bool hasFix()
+{
   uint32_t locationAge = gps.location.age();
   uint32_t altAge = gps.altitude.age();
   uint32_t satellitesAge = gps.satellites.age();
   uint32_t speedAge = gps.speed.age();
   uint32_t courseAge = gps.course.age();
-  if (locationAge > 1500 | altAge > 1500 | satellitesAge > 1500 | speedAge > 1500 | courseAge > 1500){
+  if (locationAge > 1500 | altAge > 1500 | satellitesAge > 1500 | speedAge > 1500 | courseAge > 1500)
+  {
     return false;
-  } else {
+  }
+  else
+  {
     return true;
   }
 }
